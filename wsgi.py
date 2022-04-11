f38c2a49d8091c5f94f3976641b4ca9ca71b38b7
Radosław Dębek <radekdebek2000@interia.pl>
2022-04-11 23:25:27 +0200

    Removed some files

--- wsgi.py
+++ wsgi.py
@@ -1,29 +1,38 @@
-from flask import Flask
-from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.exc import IntegrityError
-from random import randint
 import hashlib
-from flask_restful import Resource
-from flask import request, Response
+import re
 import smtplib
+from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from os import environ
+from random import randint
 
-import re
 import requests
-from datetime import datetime
-
+from flask import request, Response, Flask
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 
 AUTH_TOKEN = environ.get('AUTH_TOKEN')
 INSTA_LOGIN = environ.get('INSTA_LOGIN')
 INSTA_PASSWORD = environ.get('INSTA_PASSWORD')
+SENDER_ADDRESS = environ.get('SENDER_EMAIL')
+SENDER_PASS = environ.get('EMAIL_PASSWORD')
 
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI')
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    token = db.Column(db.String(6), nullable=True)
+
+    def __repr__(self):
+        return f'id: {self.id}, email: {self.email}, password: {self.password}, token: {self.token}'
 
 
 class InstagramApi():
@@ -44,30 +53,16 @@ class InstagramApi():
         }
 
         r = self.session.get(link)
-        csrf = re.findall(r"csrf_token\":\"(.*?)\"",r.text)[0]
-        self.session.post(login_url,data=payload,headers={
-                "user-agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
-                "x-requested-with": "XMLHttpRequest",
-                "referer": "https://www.instagram.com/accounts/login/",
-                "x-csrftoken":csrf
+        csrf = re.findall(r"csrf_token\":\"(.*?)\"", r.text)[0]
+        self.session.post(login_url, data=payload, headers={
+            "user-agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
+            "x-requested-with": "XMLHttpRequest",
+            "referer": "https://www.instagram.com/accounts/login/",
+            "x-csrftoken": csrf
         })
 
     def get_places_info(self, lat, long):
         return self.session.get(f'https://www.instagram.com/location_search/?latitude={lat}+&longitude={long}').json()
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
-    token = db.Column(db.String(6), nullable=True)
-
-    def __repr__(self):
-        return f'id: {self.id}, email: {self.email}, password: {self.password}, token: {self.token}'
-
-
-SENDER_ADDRESS = environ.get('SENDER_EMAIL')
-SENDER_PASS = environ.get('EMAIL_PASSWORD')
 
 
 class EmailVerfication(Resource):
@@ -125,9 +120,6 @@ class Instagram(Resource):
 
 
 class Login(Resource):
-    def get(self):
-        return 0
-
     def post(self):
         auth_token = request.form.get('auth')
         if not auth_token or auth_token != AUTH_TOKEN:
@@ -174,5 +166,4 @@ api.add_resource(Login, '/login')
 api.add_resource(Instagram, '/instagram')
 
 if __name__ == '__main__':
-
     app.run(debug=True)
