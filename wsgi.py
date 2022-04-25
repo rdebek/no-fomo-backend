a409eaed83a9ada49fea6d94b8865695533eb2e7
Radosław Dębek <radekdebek2000@interia.pl>
2022-04-25 17:23:13 +0200

    Added remove option to trends

--- wsgi.py
+++ wsgi.py
@@ -81,18 +81,26 @@ class Trends(Resource):
         auth_token = request.form.get('auth')
         if not auth_token or auth_token != AUTH_TOKEN:
             return Response(status=400)
+        mode = request.form['mode']
         email = request.form['email']
         trend = request.form['trend']
-        percentage = request.form['percentage']
-        token = request.form['token']
-        trend = Trend(email=email, trend=trend, percentage=percentage, token=token)
-        try:
-            db.session.add(trend)
-            db.session.commit()
-        except:
-            print('error')
-            return Response(json.dumps({'status': 'error'}), status=400)
-        return Response(json.dumps({'status': 'ok'}), status=200)
+        if mode == 'add':
+            percentage = request.form['percentage']
+            token = request.form['token']
+            trend = Trend(email=email, trend=trend, percentage=percentage, token=token)
+            try:
+                db.session.add(trend)
+                db.session.commit()
+            except:
+                return Response(json.dumps({'status': 'error'}), status=400)
+            return Response(json.dumps({'status': 'ok'}), status=200)
+        elif mode == 'remove':
+            try:
+                Trend.query.filter_by(email=email, trend=trend).first().delete()
+                db.session.commit()
+            except:
+                return Response(json.dumps({'status': 'error'}), status=400)
+            return Response(json.dumps({'status': 'ok'}))
 
 
 class TwitterApi:
